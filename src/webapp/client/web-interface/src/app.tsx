852946e60c4a3d53d8db52df724303c1383a601a
Lennard van der Plas <lennardplas@gmail.com>
2024-08-17 14:04:01 +0200

    feat(webapp): add name

--- src/webapp/client/web-interface/src/app.tsx
+++ src/webapp/client/web-interface/src/app.tsx
@@ -5,7 +5,7 @@ import AuthProvider from './providers/auth-provider'
 function App() {
   return (
     <>
-    <h1>Todo List</h1>
+    <h1>Todo List - Lennard van der Plas</h1>
       <AuthProvider>
         <Status />
         <TaskDashboard />
