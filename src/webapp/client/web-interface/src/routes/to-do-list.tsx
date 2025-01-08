7a2e89f70db3e0eea41b0bec00abc3673b3d32ab
Lennard van der Plas <lennardplas@gmail.com>
2025-01-08 08:45:49 +0100

    feat(webapp): add selected nav

--- src/webapp/client/web-interface/src/routes/to-do-list.tsx
+++ src/webapp/client/web-interface/src/routes/to-do-list.tsx
@@ -1,14 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
 import AuthProvider from '../providers/auth-provider'
-import Status from '../components/status'
+import Status from '../components/auth/status'
 import { TaskDashboard } from '../components/task/task-dashboard'
 
 export const Route = createFileRoute('/to-do-list')({
-  component: () => <>
-  <h1>Todo List - Lennard van der Plas</h1>
-    <AuthProvider>
-      <Status />
-      <TaskDashboard />
-    </AuthProvider>
-  </>
+  component: () => (
+    <>
+      <h1>Todo List - Lennard van der Plas</h1>
+      <AuthProvider>
+        <Status />
+        <TaskDashboard />
+      </AuthProvider>
+    </>
+  )
 })
