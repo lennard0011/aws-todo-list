6718d3b3ab76e29c5d0aabcbef8f9a517e8b8baa
Lennard van der Plas <lennardplas@gmail.com>
2025-07-31 22:25:48 +0200

    feat(webapp): Add AWS blog
    Added a new blog post detailing the journey to earning AWS Solutions Architect and Developer Associate certifications. Updated the blog index to include the new post.

--- src/webapp/client/web-interface/src/articles/posts/0005-aws-solutions-architect-developer-associate-certificates.tsx
+++ src/webapp/client/web-interface/src/articles/posts/0005-aws-solutions-architect-developer-associate-certificates.tsx
@@ -1,14 +1,45 @@
 import type { Article } from '../article.type'
 
 export const Blog: () => Article = () => {
-  const url = '/blog/posts/0005-aws-solutions-architect-developer-associate-certificates'
+  const url =
+    '/blog/posts/0005-aws-solutions-architect-developer-associate-certificates'
   const title = 'AWS Solutions Architect and Developer Associate Certificates'
-  const summary = 'My journey to earning AWS Solutions Architect and Developer Associate certifications'
+  const summary =
+    'Successfully earning AWS Solutions Architect and Developer Associate certifications'
   const releaseDate = new Date('2025-01-15')
   const content = (
     <>
       <p>
-        [Your content will go here]
+        After more than half a year, I successfully earned the AWS Solutions
+        Architect and Developer Associate certifications. In this journey, I
+        have learned a lot about AWS. Especially services I don't interact with
+        in my day to day work. More importantly, I learned a lot about myself
+        and how I can discipline myself. For the last eight months, most
+        mornings were spent studying and watching videos. In the meantime I
+        would see the progress bar slowly move to the right.
+      </p>
+
+      <h2>Why did I do this?</h2>
+      <p>
+        I wanted to deepen my understanding of AWS and its services. I also
+        wanted to challenge myself and prove my skills in cloud computing. The
+        certifications are a great way to validate my knowledge and enhance my
+        career prospects.
+      </p>
+
+      <h2>What did I learn?</h2>
+      <p>
+        I learned a lot about AWS. Especially services I don't interact with in
+        my day to day work. More importantly, I learned a lot about myself and
+        how I can discipline myself. For the last eight months, most mornings
+        were spent studying and watching videos. In the meantime I would see the
+        progress bar slowly move to the right.
+      </p>
+      <h2>What is next?</h2>
+      <p>
+        I plan to continue my AWS journey by exploring more advanced topics and
+        services. Additionally, I want to share my knowledge with others and
+        help them on their own AWS journeys.
       </p>
     </>
   )
--- src/webapp/client/web-interface/src/routes/blog/index.tsx
+++ src/webapp/client/web-interface/src/routes/blog/index.tsx
@@ -5,9 +5,10 @@ import { Blog as blog1 } from '../../articles/posts/0001-building-aws-cloud-resu
 import { Blog as blog2 } from '../../articles/posts/0002-learning-for-the-solution-architect-certificate'
 import { Blog as blog3 } from '../../articles/posts/0003-how-to-get-the-most-out-of-aws-summit'
 import { Blog as blog4 } from '../../articles/posts/0004-amazon-q-developer-game-builder-challenge'
+import { Blog as blog5 } from '../../articles/posts/0005-aws-solutions-architect-developer-associate-certificates'
 import { BlogCard } from '../../components/blog/blog-card'
 
-const blogs: Article[] = [blog1(), blog2(), blog3(), blog4()]
+const blogs: Article[] = [blog1(), blog2(), blog3(), blog4(), blog5()]
 
 const sortOnReleaseDate = (blogA: Article, blogB: Article) =>
   blogA.releaseDate > blogB.releaseDate ? -1 : 1
